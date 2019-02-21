63749311b3d1e6507168a5e47d7b33b9e3d5ca58
c3to <c3to@web.de>
2019-02-21 14:12:42 +0100

    working dummy

--- src/script.js
+++ src/script.js
@@ -1,3 +1,13 @@
 import "./style.scss";
 
 console.log("### working ###");
+
+document.getElementById('categories-open').addEventListener('click', () => {
+	console.log('click');
+	document.body.classList.toggle('menu-open');
+});
+
+document.getElementById('close-btn').addEventListener('click', () => {
+	console.log('click');
+	document.body.classList.toggle('menu-open');
+});
