d99f317120785e412df2f63c56ff0d1701d760d9
c3to <c3to@web.de>
2019-02-21 11:33:32 +0100

    webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    script: "./src/script.js",
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "dist",
+    filename: "[name].min.js",
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
+    },{
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'postcss-loader', 'sass-loader'],
+        // publicPath: '/dist',
+      })
+    }]
+  },
+  devServer: {
+    stats: "errors-only",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Probearbeit',
+      hash: true,
+      template: './src/index.html',
+      filename: './../index.html',
+      minify: {
+        collapseWhitespace: true
+      },
+    }),
+    new ExtractTextPlugin('style.css')
+  ]
+};
