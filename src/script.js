2c34934bcb5fe48ee5e4bdede05d6e1d1e8f3494
c3to <c3to@web.de>
2019-02-21 18:52:28 +0100

    final-1

--- src/script.js
+++ src/script.js
@@ -2,12 +2,67 @@ import "./style.scss";
 
 console.log("### working ###");
 
+
 document.getElementById('categories-open').addEventListener('click', () => {
-	console.log('click');
-	document.body.classList.toggle('menu-open');
+	console.log('click open');
+
+	getContent('https://jsonplaceholder.typicode.com/comments', (data) => {
+		buildMenu(data);
+		document.body.classList.toggle('menu-open');
+	})
 });
 
 document.getElementById('close-btn').addEventListener('click', () => {
-	console.log('click');
+	console.log('click close');
 	document.body.classList.toggle('menu-open');
 });
+
+function buildMenu(data) {
+	//console.log(data);
+	let categoriesMenu = document.getElementById('categories-menu');
+	categoriesMenu.innerHTML = "";
+
+	data.map( (item, index) => {
+		let node = document.createElement('div');
+		node.classList.add('item');
+
+		let btn = document.createElement('div');
+		btn.classList.add('btn-' + index);
+		btn.innerHTML = item.name; 
+
+		btn.addEventListener("mouseover", (evt) => {
+			buildContent(item.name, item.body)
+		});
+
+		node.appendChild(btn);
+		categoriesMenu.appendChild(node);
+	});
+
+	buildContent(data[0].name, data[0].body);
+}
+
+function buildContent(headline, content) {
+	let categoriesContent = document.getElementById('categories-content');
+	categoriesContent.innerHTML = `
+		<div class="content">
+			<h2>${headline}</h2>
+			<p>${content}</p>
+		</div>
+	`;
+}
+
+function getContent(url,callback) {
+  fetch(url)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+  	// reduce array to 10
+    callback(data.filter( (value, index, array) => {
+    	return index < 10;
+    }));
+  })
+  .catch((err) => {
+    console.log('>> api:', err);
+  });
+}
